c0d8cbe17ac8a897ca6e1979b3e5c9f85941b67d
Naturealizor <31334963+Naturealizor@users.noreply.github.com>
2023-05-01 00:31:02 -0400

    Wall of Best of Fuckface initial git commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,79 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  // import tile
+  import Tile from "./Tile.svelte";
+
+  // create array of audio sources
+  const tiles = [
+    { audioSrc: "src/FckFace Wall/FckfaceWall_Cut1.mp3" },
+    { audioSrc: "src/FckFace Wall/FckfaceWall_Cut2.mp3" },
+    { audioSrc: "src/FckFace Wall/FckfaceWall_Cut3.mp3" },
+    { audioSrc: "src/FckFace Wall/FckfaceWall_Cut4.mp3" },
+  ];
+
+  let titles = [
+    { id: "Cut 1", name: "first fart" },
+    { id: "Cut 2", name: "freebirds" },
+    { id: "Cut 3", name: "biology homework" },
+  ];
+</script>
+
+<h1 class="centered-text">Welcome to the Wall of Best of Fu*kFace!</h1>
+<p class="centered-text">
+  Listen to the original here <a
+    href="https://www.youtube.com/watch?v=_5v7MpXPfjk">here!</a
+  > (Warning!! If you don't think fart jokes are funny, you may want to leave now)
+</p>
+<p class="centered-text">
+  I really can't say why exactly I wanted to make this, other than I got bored
+  and this popped into my head. I hope you enjoy it!
+</p>
+
+<div class="container">
+  {#each tiles as tile, i}
+    <div class="card">
+      <Tile audioSrc={tile.audioSrc} title={titles[i]} />
+      <!-- {titles.forEach(function (titles) {
+        console.log(titles); -->
+      <!-- })} -->
+      <!-- {#each titles as title, i}
+        <div class="title">
+          {i + 1}:
+          <p>{title.id}: {title.name}</p>
+        </div>
+      {/each} -->
+    </div>
+  {/each}
+</div>
+
+<style>
+  /* //create centered text class */
+  .centered-text {
+    text-align: center;
+  }
+  p {
+    font-size: 1.5rem;
+  }
+  .container {
+    padding-top: 5%;
+    margin: auto;
+    display: grid;
+    grid-template-columns: repeat(3, 1fr);
+    justify-items: center;
+    align-items: center;
+    gap: 1rem;
+    width: 50%;
+  }
+  .card {
+    width: 50%;
+    height: 50%;
+    background-color: #f5f5f5;
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 1rem;
+    gap: 2rem;
+  }
+</style>
---
+++ src/routes/Tile.svelte
@@ -0,0 +1,46 @@
+<script>
+  import { onMount } from "svelte";
+
+  export let audioSrc;
+  let audio;
+
+  // let count = 0;
+  // let newCount = (count += 1);
+  // let titleStart = "Cut ";
+
+  // let title = titleStart + newCount;
+  // console.log(title);
+
+  const titles = [
+    { name: "first fart" },
+    { name: "freebirds" },
+    { name: "biology homework" },
+  ];
+
+  onMount(() => {
+    audio = new Audio(audioSrc);
+  });
+
+  function playAudio() {
+    audio.play();
+  }
+
+  function showTitle() {
+    const tileElements = [];
+
+    for (let i = 0; i < titles.length; i++) {
+      const title = titles[i];
+      tileElements.push({ title });
+    }
+    console.log(tileElements);
+    return tileElements;
+  }
+</script>
+
+<div on:mouseover={playAudio} on:focus={playAudio}>
+  {#each titles as title, i}
+    <div class="tile">
+      <p>{title.forEach(title)}</p>
+    </div>
+  {/each}
+</div>
